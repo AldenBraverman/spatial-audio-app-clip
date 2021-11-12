8cfe332e21f251e36731ab7cd1424ad16c2560f8
Alden Braverman <aldenbrave@aol.com>
2021-11-11 22:56:22 -0500

    Airpods Tracking Test
    should print rotation values

--- spatial-audio-app-clip/ViewController.swift
+++ spatial-audio-app-clip/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import CoreMotion
 
-class ViewController: UIViewController {
+var degreesYaw = 0.0
+var degreesPitch = 0.0
+var degreesRoll = 0.0
+
+var yawEnabled = true
+var pitchEnabled = false
+var rollEnabled = false
+
+class ViewController: UITableViewController, CMHeadphoneMotionManagerDelegate {
+    
+    @IBOutlet var pitchValue: UILabel!
+    @IBOutlet var rollValue: UILabel!
+    @IBOutlet var yawValue: UILabel!
+
+    @IBAction func enablePitch(_ sender: Any) {
+        pitchEnabled = !pitchEnabled
+        if(!pitchEnabled){
+            degreesPitch = 0
+        }
+    }
+    @IBAction func enableRoll(_ sender: Any) {
+        rollEnabled = !rollEnabled
+        if(!rollEnabled){
+            degreesRoll = 0
+        }
+    }
+    @IBAction func enableYaw(_ sender: Any) {
+        yawEnabled = !yawEnabled
+        if(!yawEnabled){
+            degreesYaw = 0
+        }
+    }
+    
+    var motionManager: CMHeadphoneMotionManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        motionManager = CMHeadphoneMotionManager()
+        motionManager.delegate = self
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { (motion, error) in
+                
+                if (yawEnabled){
+                    degreesYaw = -(motion?.attitude.yaw)! * 180 / Double.pi
+                } else {
+                    degreesYaw = 0;
+                }
+                if (pitchEnabled){
+                    degreesPitch = (motion?.attitude.pitch)! * 180 / Double.pi
+                } else {
+                    degreesPitch = 0;
+                }
+                if (rollEnabled){
+                    degreesRoll = (motion?.attitude.roll)! * 180 / Double.pi
+                } else {
+                    degreesRoll = 0;
+                }
+                
+                self.yawValue.text = "\(degreesYaw)"
+                self.pitchValue.text = "\(degreesPitch)"
+                self.rollValue.text = "\(degreesRoll)"
+            }
+        }
     }
-
-
 }
-
